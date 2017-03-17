cac76b8bd00920f9fa11f6e3000b32742bfabc3d
Mladen Ignjatovic <mladen@localhost.localdomain>
2017-03-17 13:07:19 +0100

    config changed

--- app/Models/Database.php
+++ app/Models/Database.php
@@ -8,11 +8,17 @@ class Database {
 	function __construct ($f3) {
 
 
+	if (is_object($db)) {
+			$this->db = $db;
+	}
+	else {
+
 		$this->db = new DB\SQL(
 			    $f3->get('dsn'),
 			    $f3->get('db_user'),
 			    $f3->get('db_pass')
 			);
+	 	}
 	}
 
 }
--- tmp/1vtc520zcyudm.30wdr7uzoawww.php
+++ tmp/1vtc520zcyudm.30wdr7uzoawww.php
@@ -7,5 +7,5 @@
 	});
 </script>
 <footer class="uk-section uk-section-secondary uk-margin-remove-bottom uk-padding-small">
-	<p class="uk-text-center">Simple Blogging system by Mladen Ignjatovic. Not for production use</p>
+	<p class="uk-text-center">Simple Blogging system by Mladen Ignjatovic. Not for production usage</p>
 </footer>
